c24efd0183b52d28056cc7b92f92ee290ee61689
Abdul-wadudsumaila <88601172+Abdul-wadudsumaila@users.noreply.github.com>
2021-08-16 02:23:02 +0000

    Dog Years Project

---
+++ dogyears/dogYears.js
@@ -0,0 +1,9 @@
+let myAge =25; //this declares age
+let earlyYears = 2; //this variable changes
+earlyYears *= 10.5;
+laterYears = myAge - 2;//This calculates the later years
+laterYears *= 4;
+//this sums the early and later years together
+myAgeInDogYear = earlyYears + laterYears;
+var myName = 'Kofi'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYear} years old in dog years`)
